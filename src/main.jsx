d37d9c91179899ed6a989201bd99ae334d0dc7fc
itsvivekg10 <itsvivekg10@gmail.com>
2024-10-06 21:49:20 +0530

    error resolved

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
-import Home from "./home";
+import Home from "./Home";
 import Contact from "./Contact";
 import Profile from "./profile";
 import AddListing from "./add-listing";
