b622f61ce947af7df1f16e7ab9e0f3fb5011e2b5
itsvivekg10 <itsvivekg10@gmail.com>
2024-10-09 15:35:56 +0530

    configs changes

--- configs/index.js
+++ configs/index.js
@@ -1,5 +1,20 @@
+// require("dotenv").config();
+
+// import { neon } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
+// import * as schema from "./schema";
+// const sql = neon(import.meta.env.VITE_DRIZZLE_DATABASE_URL);
+// export const db = drizzle(sql, { schema });
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-const sql = neon(import.meta.env.VITE_DRIZZLE_DATABASE_URL);
+
+// Access the environment variable using import.meta.env
+const connectionString = import.meta.env.VITE_DRIZZLE_DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("No database connection string provided.");
+}
+
+const sql = neon(connectionString);
 export const db = drizzle(sql, { schema });
